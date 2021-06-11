c0de179942c9a31b544d9a261e40ef7e3afc972d
Michaux <michaux2817@gmail.com>
2021-06-11 00:09:06 -0700

    changed all .js files for Main page to .jsx files; deleted css folder; deleted Header.js component and implemented a responsive Navbar component

--- frens-web/src/containers/Register Page/index.jsx
+++ frens-web/src/containers/Register Page/index.jsx
@@ -1,9 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import {useSelector} from "react-redux";
+import React, { useRef, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 import gsap, { Power3 } from "gsap";
 
-import styled, {ThemeProvider} from  "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "../../themes.js";
 
 import { PageContainer } from "../../components/PageContainer";
@@ -16,25 +16,24 @@ import SlipIn from "../../components/SlipIn";
 import Footer from "../../components/Footer";
 
 const RegisterContainer = styled.div`
-  visibility: hidden;
-//   width: 100%;
-//   height: 100vh;
-  padding: 0;
-  margin: 0;
-  min-height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-//   background-color: var(--black);
+    visibility: hidden;
+    //   width: 100%;
+    //   height: 100vh;
+    padding: 0;
+    margin: 0;
+    min-height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    //   background-color: var(--black);
 `;
 
 const RegisterPage = () => {
     let theme = "light";
     // Check redux isDark state
-    const isDark = useSelector(state => state.isDark);
-    if(isDark) {
-
+    const isDark = useSelector((state) => state.isDark);
+    if (isDark) {
         theme = "dark";
     } else {
         theme = "light";
@@ -43,40 +42,37 @@ const RegisterPage = () => {
     let content = useRef(null);
     let middleSection = useRef(null);
     let head = useRef(null);
-    let tl = new gsap.timeline;
+    let tl = new gsap.timeline();
 
-    useEffect( () => {
+    useEffect(() => {
         // Wait for page to load, prevent glitches
-        gsap.to(content, 0, {css: {visibility: 'visible'}})
+        gsap.to(content, 0, { css: { visibility: "visible" } });
 
-        tl.from(head, 1, {x:-1000, ease: Power3.easeOut})
-          .from(middleSection, 1.2, {y: -1080, ease: Power3.easeOut},0.2);
-    })
+        tl.from(head, 1, { x: -1000, ease: Power3.easeOut }).from(middleSection, 1.2, { y: -1080, ease: Power3.easeOut }, 0.2);
+    });
 
     return (
         <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
             <GlobalStyles />
-                <RegisterContainer ref={el => content = el}>
-                    <PageContainer>
-                        <HeaderWrapper>
-                            <div ref={el => head = el}>
-                                <SlipIn name="Register"/>
-                            </div>
-                        </HeaderWrapper>
-                        
-                        <ContentWrapper>
-                            <div ref={el => middleSection = el} >
-                                <MiddleUI/>
-                            </div>
-                        </ContentWrapper>
+            <RegisterContainer ref={(el) => (content = el)}>
+                <PageContainer>
+                    <HeaderWrapper>
+                        <div ref={(el) => (head = el)}>
+                            <SlipIn name="Register" />
+                        </div>
+                    </HeaderWrapper>
 
-                        <FooterWrapper>
-                            <Footer/>
-                        </FooterWrapper>
+                    <ContentWrapper>
+                        <div ref={(el) => (middleSection = el)}>
+                            <MiddleUI />
+                        </div>
+                    </ContentWrapper>
 
-                    </PageContainer>
-                    
-                </RegisterContainer>
+                    <FooterWrapper>
+                        <Footer />
+                    </FooterWrapper>
+                </PageContainer>
+            </RegisterContainer>
         </ThemeProvider>
     );
 };
